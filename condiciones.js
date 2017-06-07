c1ba28b2a7b240ed55706b6608073e44724d97e7
Isaac Daniel Batista <disa.indus@gmail.com>
2017-06-07 15:37:21 -0500

    condiciones y tipos de variables (let, const)

--- condiciones.js
+++ condiciones.js
@@ -2,14 +2,17 @@ const starWars7 = 'Star Wars: El despertar de la Fuerza';
 const pgStarWars7 = 13;
 
 const name = 'Daniel'
-const age  = 30;
+let age  = 30;
 const name2 = 'Isaac'
-const age2  = 11;
+let age2  = 11;
 
-function canWatchStartWars7 (name, age)
+function canWatchStartWars7 (name, age, isWithAdult = false)
 {
 	if (age >= pgStarWars7) {
 		alert(`${name} puede pasar a ver ${starWars7}`);
+	} elseif(isWithAdult) {
+		alert(`${name} puede pasar a ver ${starWars7}.
+			Aunque su edad es ${age}, se encuentra acompañada/o por un adulto.`);
 	} else {
 		alert(`${name} no puede pasar a ver ${starWars7}.
 			Tiene ${age} años y necesita tener ${pgStarWars7}`);
@@ -18,3 +21,5 @@ function canWatchStartWars7 (name, age)
 
 canWatchStartWars7(name, age)
 canWatchStartWars7(name2, age2)
+canWatchStartWars7(name2, age2, true)
+
