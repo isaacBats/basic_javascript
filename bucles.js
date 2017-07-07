7b6e6a119a236b2a9ed43c7f779df465660ddbf0
Isaac Daniel Batista <disa.indus@gmail.com>
2017-07-07 13:38:21 -0500

    Ciclo While

--- bucles.js
+++ bucles.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// Cuanto corro
 const dias = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo']
 const name = 'Isaac'
 
@@ -21,3 +22,39 @@ for (let i = 0; i < length; i++) {
 
 const promedioKms = totalKms / length
 console.log(`En promedio, ${name} corrió ${promedioKms.toFixed(2)}kms`)
+
+// Pelea entre Goku y Superman jeje
+let vidaGoku = 100
+let vidaSuperman = 100
+
+const MIN_POWER = 5
+const MAX_POWER = 12
+
+const ambosSiguenVivos = () => vidaGoku > 0 && vidaSuperman > 0
+const calcularGolpe = () => Math.round(Math.random() * (MAX_POWER - MIN_POWER)) + MIN_POWER
+const gokuSigueVivo = () => vidaGoku > 0
+let round = 0
+
+while(ambosSiguenVivos()) {
+	round++
+	console.log(`Round ${round}`)
+	const golpeGoku = calcularGolpe()
+	const golpeSuperman = calcularGolpe()
+
+	if(golpeGoku > golpeSuperman) {
+		// ataca Goku
+		console.log(`Goku ataca a Superman con un golpe de ${golpeGoku}`)
+		vidaSuperman -= golpeGoku
+		console.log(`Superman queda en ${vidaSuperman} de vida`)
+	} else {
+		console.log(`Superman ataca a Goku con un golpe de ${golpeSuperman}`)
+		vidaGoku -= golpeSuperman
+		console.log(`Goku queda en ${vidaGoku} de vida`)
+	}
+}
+
+if(gokuSigueVivo()) {
+	console.log(`Goku ganó la pelea. Su vida es de: ${vidaGoku}`)
+} else {
+	console.log(`Superman ganó la pelea. Su vida es de: ${vidaSuperman}`)
+}
